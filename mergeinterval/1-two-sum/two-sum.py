82b1a66e4ae536a31b7ec31716c371c6626ca520
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2026-04-04 14:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (50.85%) - LeetSync

--- mergeinterval/1-two-sum/two-sum.py
+++ mergeinterval/1-two-sum/two-sum.py
@@ -1,8 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+
+        for i,num in enumerate(nums):
+            if target-num in dict:
+                return [dict[target-num],i]
+            else:
+                dict[num]=i
+
+       
         
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
         
