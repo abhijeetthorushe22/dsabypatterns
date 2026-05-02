a10cb88caace1168c36689da5e6ccba01679167d
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2026-05-02 18:38:09 +0530

    Time: 8 ms (26.23%) | Memory: 20.4 MB (42.10%) - LeetSync

---
+++ mergeinterval/268-missing-number/missing-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        i = 0
+        n = len(nums)
+        
+        while i < n:
+            correct = nums[i]
+            
+            if nums[i] < n and nums[i] != nums[correct]:
+                nums[i], nums[correct] = nums[correct], nums[i]
+            else:
+                i += 1
+        
+        for i in range(n):
+            if nums[i] != i:
+                return i
+        
+        return n
+        
